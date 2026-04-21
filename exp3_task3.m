6c53d4433b1db7798ad83d8880d5d211da2b1f8f
Mihir Naik <mihirnaik832@gmail.com>
2026-04-21 23:43:41 +0530

    Delete exp3_task3.m

--- exp3_task3.m
+++
@@ -1,7 +0,0 @@
-n = [1 : 25]
-x = cos(0.2*pi*n) + cos(0.22*pi*n) + cos(0.6*pi*n)
-%absolute fourier transform and then phase fourier transform 
-stem(abs(fft(x,1024))); 
-xlabel('Frequency'); 
-ylabel('Amplitude'); 
-title('Absolute Fourier Transform')
